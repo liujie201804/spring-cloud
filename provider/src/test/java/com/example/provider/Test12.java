337c1638a97ec81e28473afebe7ea997aeac2a36
jieli <liujie111>
2020-05-14 16:06:23 +0800

    添加测试用例依赖

---
+++ provider/src/test/java/com/example/provider/Test12.java
@@ -0,0 +1,20 @@
+package com.example.provider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author: jieliu
+ * @title: Test
+ * @description:
+ * @date: 2020-05-14 13:58
+ **/
+@SpringBootTest
+public class Test12 {
+
+    @Test
+    void contest(){
+        System.out.println("eee");
+    }
+
+}
