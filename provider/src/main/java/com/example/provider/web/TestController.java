11c1ec91f08e2fcb8ee887a4b90645aa17468dfa
jieli <liujie111>
2020-05-20 09:25:11 +0800

    添加lombok依赖

---
+++ provider/src/main/java/com/example/provider/web/TestController.java
@@ -0,0 +1,15 @@
+package com.example.provider.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: jieliu
+ * @title: TestController
+ * @description:
+ * @date: 2020-05-20 09:23
+ **/
+@RequestMapping("test")
+@RestController
+public class TestController {
+}
